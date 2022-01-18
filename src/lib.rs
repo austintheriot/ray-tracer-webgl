faec8fe0630a240ebfc1f59cac7e20ac67dc7742
Austin Theriot <austinmtheriot@gmail.com>
2022-01-17 21:15:20 -0600

    import glsl scripts from html file

--- src/lib.rs
+++ src/lib.rs
@@ -1,83 +1,16 @@
 #![feature(format_args_capture)]
 extern crate console_error_panic_hook;
+
+use log::info;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use web_sys::{WebGlProgram, WebGlRenderingContext, WebGlShader};
+use web_sys::{HtmlScriptElement, WebGlProgram, WebGlRenderingContext, WebGlShader};
 
 pub const WIDTH: u32 = 1600;
 pub const HEIGHT: u32 = 900;
 pub const VERTICES: [f32; 12] = [
     -1.0, 1.0, 1.0, 1.0, -1.0, -1.0, -1.0, -1.0, 1.0, 1.0, 1.0, -1.0,
 ];
-
-#[wasm_bindgen]
-pub fn main() -> Result<(), JsValue> {
-    // enables ore helpful stack traces
-    console_error_panic_hook::set_once();
-    wasm_logger::init(wasm_logger::Config::default());
-
-    // GET ELEMENTS
-    let document = web_sys::window().unwrap().document().unwrap();
-    let canvas = document
-        .query_selector("canvas")?
-        .unwrap()
-        .dyn_into::<web_sys::HtmlCanvasElement>()?;
-
-    let ctx = canvas
-        .get_context("webgl")?
-        .unwrap()
-        .dyn_into::<WebGlRenderingContext>()?;
-
-    canvas.set_attribute("width", &WIDTH.to_string())?;
-    canvas.set_attribute("height", &HEIGHT.to_string())?;
-
-    // INIT PROGRAM
-    let vert_shader = compile_shader(
-        &ctx,
-        WebGlRenderingContext::VERTEX_SHADER,
-        r#"
-        attribute vec4 position;
-        void main() {
-            gl_Position = position;
-        }
-    "#,
-    )?;
-    let frag_shader = compile_shader(
-        &ctx,
-        WebGlRenderingContext::FRAGMENT_SHADER,
-        r#"
-        void main() {
-            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-        }
-    "#,
-    )?;
-    let program = link_program(&ctx, &vert_shader, &frag_shader)?;
-    ctx.use_program(Some(&program));
-
-    // SETUP VERTEX BUFFER
-    let buffer = ctx.create_buffer().ok_or("failed to create buffer")?;
-    ctx.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&buffer));
-    let vert_array = unsafe { js_sys::Float32Array::view(&VERTICES) };
-    ctx.buffer_data_with_array_buffer_view(
-        WebGlRenderingContext::ARRAY_BUFFER,
-        &vert_array,
-        WebGlRenderingContext::STATIC_DRAW,
-    );
-    ctx.vertex_attrib_pointer_with_i32(0, 2, WebGlRenderingContext::FLOAT, false, 0, 0);
-    ctx.enable_vertex_attrib_array(0);
-
-    // RENDER
-    ctx.clear_color(0.0, 0.0, 0.0, 1.0);
-    ctx.clear(WebGlRenderingContext::COLOR_BUFFER_BIT);
-    ctx.viewport(0, 0, canvas.width() as i32, canvas.height() as i32);
-    ctx.draw_arrays(
-        WebGlRenderingContext::TRIANGLES,
-        0,
-        (VERTICES.len() / 2) as i32,
-    );
-
-    Ok(())
-}
 
 pub fn compile_shader(
     ctx: &WebGlRenderingContext,
@@ -127,4 +60,72 @@ pub fn link_program(
             .get_program_info_log(&program)
             .unwrap_or_else(|| String::from("Unknown error creating program object")))
     }
+}
+
+/// Entry function cannot be async, so spawns a local Future for running the real main function
+#[wasm_bindgen]
+pub fn main() -> Result<(), JsValue> {
+    // enables ore helpful stack traces
+    console_error_panic_hook::set_once();
+    wasm_logger::init(wasm_logger::Config::default());
+
+    // GET ELEMENTS
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+    let canvas = document
+        .query_selector("canvas")?
+        .unwrap()
+        .dyn_into::<web_sys::HtmlCanvasElement>()?;
+
+    let ctx = canvas
+        .get_context("webgl")?
+        .unwrap()
+        .dyn_into::<WebGlRenderingContext>()?;
+
+    canvas.set_attribute("width", &WIDTH.to_string())?;
+    canvas.set_attribute("height", &HEIGHT.to_string())?;
+
+    //  SETUP PROGRAM
+    let vertex_shader = {
+        let shader_source = &document
+            .query_selector("#vertex-shader")?
+            .unwrap()
+            .dyn_into::<HtmlScriptElement>()?
+            .text()?;
+        compile_shader(&ctx, WebGlRenderingContext::VERTEX_SHADER, shader_source).unwrap()
+    };
+    let fragment_shader = {
+        let shader_source = &document
+            .query_selector("#fragment-shader")?
+            .unwrap()
+            .dyn_into::<HtmlScriptElement>()?
+            .text()?;
+        compile_shader(&ctx, WebGlRenderingContext::FRAGMENT_SHADER, shader_source).unwrap()
+    };
+    let program = link_program(&ctx, &vertex_shader, &fragment_shader)?;
+    ctx.use_program(Some(&program));
+
+    // SETUP VERTEX BUFFER
+    let buffer = ctx.create_buffer().ok_or("failed to create buffer")?;
+    ctx.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&buffer));
+    let vert_array = unsafe { js_sys::Float32Array::view(&VERTICES) };
+    ctx.buffer_data_with_array_buffer_view(
+        WebGlRenderingContext::ARRAY_BUFFER,
+        &vert_array,
+        WebGlRenderingContext::STATIC_DRAW,
+    );
+    ctx.vertex_attrib_pointer_with_i32(0, 2, WebGlRenderingContext::FLOAT, false, 0, 0);
+    ctx.enable_vertex_attrib_array(0);
+
+    // RENDER
+    ctx.clear_color(0.0, 0.0, 0.0, 1.0);
+    ctx.clear(WebGlRenderingContext::COLOR_BUFFER_BIT);
+    ctx.viewport(0, 0, canvas.width() as i32, canvas.height() as i32);
+    ctx.draw_arrays(
+        WebGlRenderingContext::TRIANGLES,
+        0,
+        (VERTICES.len() / 2) as i32,
+    );
+
+    Ok(())
 }
