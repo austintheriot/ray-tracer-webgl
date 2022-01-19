9aa0d93ba722216c6492997099426a9ca762deff
Austin Theriot <austinmtheriot@gmail.com>
2022-01-18 21:58:33 -0600

    find middle ground on quality vs render speed

--- src/lib.rs
+++ src/lib.rs
@@ -14,12 +14,12 @@ use web_sys::{
     WebGlShader, WebGlTexture,
 };
 
-pub const WIDTH: u32 = 1600;
-pub const HEIGHT: u32 = 900;
+pub const WIDTH: u32 = 1280;
+pub const HEIGHT: u32 = 720;
 pub const BYTES_PER_PIXEL: u32 = 4;
 pub const PIXEL_ARRAY_LENGTH: usize = (WIDTH * HEIGHT * BYTES_PER_PIXEL) as usize;
 pub const ASPECT_RATIO: f64 = (WIDTH as f64) / (HEIGHT as f64);
-pub const SAMPLES_PER_PIXEL: u32 = 1;
+pub const SAMPLES_PER_PIXEL: u32 = 3;
 pub const MAX_DEPTH: u32 = 5;
 pub const VIEWPORT_HEIGHT: f64 = 2.0;
 pub const VIEWPORT_WIDTH: f64 = ASPECT_RATIO * VIEWPORT_HEIGHT;
