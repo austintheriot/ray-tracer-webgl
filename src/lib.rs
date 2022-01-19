6abfd09b6cb2e2d900c8ac405b12f89e7510270d
Austin Theriot <austinmtheriot@gmail.com>
2022-01-19 11:56:09 -0600

    move global state into a struct

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,9 @@ mod vec3;
 use std::cell::RefCell;
 use std::f64::consts::PI;
 use std::rc::Rc;
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::sync::MutexGuard;
 use vec3::{Point, Vec3};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
@@ -18,51 +21,100 @@ use web_sys::{
     WebGlShader, WebGlTexture,
 };
 
-pub const WIDTH: u32 = 2048;
-pub const HEIGHT: u32 = 1152;
 pub const BYTES_PER_PIXEL: u32 = 4;
-pub const PIXEL_ARRAY_LENGTH: usize = (WIDTH * HEIGHT * BYTES_PER_PIXEL) as usize;
-pub const ASPECT_RATIO: f64 = (WIDTH as f64) / (HEIGHT as f64);
-pub const SAMPLES_PER_PIXEL: u32 = 1;
-pub const MAX_DEPTH: u32 = 10;
-pub const FOCAL_LENGTH: f64 = 1.0;
-pub const CAMERA_ORIGIN: Point = Point(0., 0., 0.);
-pub const CAMERA_FIELD_OF_VIEW: f64 = 90.;
+
+struct State {
+    width: u32,
+    height: u32,
+    aspect_ratio: f64,
+    samples_per_pixel: u32,
+    max_depth: u32,
+    focal_length: f64,
+    camera_origin: Point,
+    camera_field_of_view: f64,
+    viewport_height: f64,
+    viewport_width: f64,
+    viewport_horizontal_vec: Vec3,
+    viewport_vertical_vec: Vec3,
+    lower_left_corner: Point,
+    /// If the render should render incrementally, averaging together previous frames
+    should_average: bool,
+    /// Unless averaging is taking place, this is set to false after revery render
+    /// only updated back to true if something changes (i.e. input)
+    should_render: bool,
+    /// Whether the browser should save a screenshot of the canvas
+    should_save: bool,
+    /// Used to alternate which framebuffer to render to
+    even_odd_count: u32,
+    /// Used for averaging previous frames together
+    render_count: f32,
+    /// The weight of the last frame compared to the each frame before.
+    last_frame_weight: f32,
+    /// Limiting the counted renders allows creating a sliding average of frames
+    max_render_count: f32,
+    prev_now: f64,
+    prev_fps_update_time: f64,
+    prev_fps: [f64; 50],
+}
+
+impl Default for State {
+    fn default() -> Self {
+        let width = 2048;
+        let height = 1152;
+        let camera_origin = Point(0., 0., 0.);
+        let aspect_ratio = (width as f64) / (height as f64);
+        let camera_field_of_view = 90.;
+        let camera_field_of_view_radians = (camera_field_of_view * PI) / 180.;
+        let camera_h = (camera_field_of_view_radians / 2.).tan();
+        let viewport_height = 2. * camera_h;
+        let viewport_width = viewport_height * aspect_ratio;
+        let viewport_horizontal_vec = Vec3(viewport_width, 0., 0.);
+        let viewport_vertical_vec = Vec3(0., viewport_height, 0.);
+        let focal_length = 1.;
+        let lower_left_corner = Vec3(
+            camera_origin.x() - viewport_horizontal_vec.0 / 2.,
+            camera_origin.y() - viewport_vertical_vec.1 / 2.,
+            camera_origin.z() - focal_length,
+        );
+
+        State {
+            width,
+            height,
+            aspect_ratio,
+            samples_per_pixel: 1,
+            max_depth: 10,
+            focal_length: 1.,
+            camera_origin,
+            camera_field_of_view,
+            viewport_height,
+            viewport_width,
+            viewport_horizontal_vec,
+            viewport_vertical_vec,
+            lower_left_corner,
+            should_average: true,
+            should_render: true,
+            should_save: false,
+            even_odd_count: 0,
+            render_count: 0.,
+            last_frame_weight: 1.,
+            max_render_count: 10000.,
+            prev_now: 0.,
+            prev_fps_update_time: 0.,
+            prev_fps: [0.; 50],
+        }
+    }
+}
+
+unsafe impl Send for State {}
+unsafe impl Sync for State {}
+
 lazy_static! {
-    static ref CAMERA_FIELD_OF_VIEW_RADIANS: f64 = (CAMERA_FIELD_OF_VIEW * PI) / 180.;
-    static ref CAMERA_H: f64 = (*CAMERA_FIELD_OF_VIEW_RADIANS / 2.).tan();
-    static ref VIEWPORT_HEIGHT: f64 = 2. * (*CAMERA_H);
-    static ref VIEWPORT_WIDTH: f64 = ASPECT_RATIO * (*VIEWPORT_HEIGHT);
-    static ref VIEWPORT_HORIZONTAL_VEC: Vec3 = Vec3(*VIEWPORT_WIDTH, 0., 0.);
-    static ref VIEWPORT_VERTICAL_VEC: Vec3 = Vec3(0., *VIEWPORT_HEIGHT, 0.);
-    static ref LOWER_LEFT_CORNER: Point = Vec3(
-        CAMERA_ORIGIN.0 - VIEWPORT_HORIZONTAL_VEC.0 / 2.,
-        CAMERA_ORIGIN.1 - VIEWPORT_VERTICAL_VEC.1 / 2.,
-        CAMERA_ORIGIN.2 - FOCAL_LENGTH,
-    );
+    static ref STATE: Arc<Mutex<State>> = Arc::new(Mutex::new(State::default()));
 }
 
 pub const SIMPLE_QUAD_VERTICES: [f32; 12] = [
     -1.0, 1.0, 1.0, 1.0, -1.0, -1.0, -1.0, -1.0, 1.0, 1.0, 1.0, -1.0,
 ];
-/// If the render should render incrementally, averaging together previous frames
-pub const SHOULD_AVERAGE: bool = true;
-/// Unless averaging is taking place, this is set to false after revery render
-/// only updated back to true if something changes (i.e. input)
-static mut SHOULD_RENDER: bool = true;
-/// Whether the browser should save a screenshot of the canvas
-static mut SHOULD_SAVE: bool = false;
-/// Used to alternate which framebuffer to render to
-static mut EVEN_ODD_COUNT: u32 = 0;
-/// Used for averaging previous frames together
-static mut RENDER_COUNT: f32 = 0.;
-/// The weight of the last frame compared to the each frame before.
-pub const LAST_FRAME_WEIGHT: f32 = 1.;
-/// Limiting the counted renders allows creating a sliding average of frames
-static mut MAX_RENDER_COUNT: f32 = 10000.;
-static mut PREV_NOW: f64 = 0.;
-static mut PREV_FPS_UPDATE_TIME: f64 = 0.;
-static mut PREV_FPS: [f64; 50] = [0.; 50];
 
 fn compile_shader(
     gl: &WebGl2RenderingContext,
@@ -114,7 +166,7 @@ fn link_program(
     }
 }
 
-fn create_texture(gl: &WebGl2RenderingContext) -> WebGlTexture {
+fn create_texture(gl: &WebGl2RenderingContext, state: &MutexGuard<State>) -> WebGlTexture {
     let texture = gl.create_texture();
     gl.bind_texture(WebGl2RenderingContext::TEXTURE_2D, texture.as_ref());
 
@@ -145,14 +197,15 @@ fn create_texture(gl: &WebGl2RenderingContext) -> WebGlTexture {
         WebGl2RenderingContext::TEXTURE_2D,
         0,
         WebGl2RenderingContext::RGBA as i32,
-        WIDTH as i32,
-        HEIGHT as i32,
+        state.width as i32,
+        state.height as i32,
         0,
         WebGl2RenderingContext::RGBA,
         WebGl2RenderingContext::UNSIGNED_BYTE,
         None,
     )
     .unwrap();
+    drop(state);
 
     texture.unwrap()
 }
@@ -189,9 +242,9 @@ fn request_animation_frame(f: &Closure<dyn FnMut()>) {
         .expect("should register `requestAnimationFrame` OK");
 }
 
-fn draw(gl: &WebGl2RenderingContext) {
+fn draw(gl: &WebGl2RenderingContext, state: &MutexGuard<State>) {
     gl.clear_color(0.0, 0.0, 0.0, 1.0);
-    gl.viewport(0, 0, WIDTH as i32, HEIGHT as i32);
+    gl.viewport(0, 0, state.width as i32, state.height as i32);
     gl.clear(WebGl2RenderingContext::COLOR_BUFFER_BIT);
     gl.draw_arrays(
         WebGl2RenderingContext::TRIANGLES,
@@ -200,46 +253,41 @@ fn draw(gl: &WebGl2RenderingContext) {
     );
 }
 
-fn update_fps_indicator(p: &HtmlParagraphElement, now: f64) {
-    unsafe {
-        if now - PREV_FPS_UPDATE_TIME > 250. {
-            PREV_FPS_UPDATE_TIME = now;
-            let average_fps: f64 = PREV_FPS.iter().sum::<f64>() / (PREV_FPS.len() as f64);
-            p.set_text_content(Some(&format!("{:.2} fps", average_fps)))
-        }
+fn update_fps_indicator(p: &HtmlParagraphElement, now: f64, state: &mut MutexGuard<State>) {
+    if now - state.prev_fps_update_time > 250. {
+        state.prev_fps_update_time = now;
+        let average_fps: f64 = state.prev_fps.iter().sum::<f64>() / (state.prev_fps.len() as f64);
+        p.set_text_content(Some(&format!("{:.2} fps", average_fps)))
     }
 }
 
-fn update_moving_fps_array(now: f64) {
-    unsafe {
-        // calculate moving fps
-        let dt = now - PREV_NOW;
-        PREV_NOW = now;
-        let fps = 1000. / dt;
-        for (i, el) in PREV_FPS.into_iter().skip(1).enumerate() {
-            PREV_FPS[i] = el;
-        }
-        PREV_FPS[PREV_FPS.len() - 1] = fps;
+fn update_moving_fps_array(now: f64, state: &mut MutexGuard<State>) {
+    // calculate moving fps
+    let dt = now - state.prev_now;
+    state.prev_now = now;
+    let fps = 1000. / dt;
+    let last_index = state.prev_fps.len() - 1;
+    for (i, el) in state.prev_fps.into_iter().skip(1).enumerate() {
+        state.prev_fps[i] = el;
     }
+    state.prev_fps[last_index] = fps;
 }
 
-fn update_render_globals() {
-    unsafe {
-        if !SHOULD_AVERAGE {
-            // only continuously render when averaging is being done
-            SHOULD_RENDER = false;
-        }
-        EVEN_ODD_COUNT += 1;
-        RENDER_COUNT = (RENDER_COUNT + 1.).min(MAX_RENDER_COUNT);
-    };
+fn update_render_globals(state: &mut MutexGuard<State>) {
+    if !state.should_average {
+        // only continuously render when averaging is being done
+        state.should_render = false;
+    }
+    state.even_odd_count += 1;
+    state.render_count = (state.render_count + 1.).min(state.max_render_count);
 }
 
 #[wasm_bindgen]
 pub fn save_image() -> Result<(), JsValue> {
-    unsafe {
-        SHOULD_RENDER = true;
-        SHOULD_SAVE = true;
-    }
+    // can take a mutex guard here, because it will never be called while render loop is running
+    let mut state = (*STATE).lock().unwrap();
+    state.should_render = true;
+    state.should_save = true;
 
     Ok(())
 }
@@ -269,8 +317,10 @@ pub fn main() -> Result<(), JsValue> {
         .unwrap()
         .dyn_into::<web_sys::HtmlParagraphElement>()?;
 
-    canvas.set_width(WIDTH);
-    canvas.set_height(HEIGHT);
+    let state = (*STATE).lock().unwrap();
+    canvas.set_width(state.width);
+    canvas.set_height(state.height);
+    drop(state);
 
     //  SETUP PROGRAM
     let vertex_shader = {
@@ -317,6 +367,8 @@ pub fn main() -> Result<(), JsValue> {
     // SET VERTEX BUFFER
     let buffer = gl.create_buffer().ok_or("failed to create buffer")?;
     gl.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(&buffer));
+    // requires `unsafe` since we're creating a raw view into wasm memory,
+    // but this array is static, so it shouldn't cause any issues
     let vertex_array = unsafe { js_sys::Float32Array::view(&SIMPLE_QUAD_VERTICES) };
     gl.buffer_data_with_array_buffer_view(
         WebGl2RenderingContext::ARRAY_BUFFER,
@@ -334,7 +386,9 @@ pub fn main() -> Result<(), JsValue> {
     );
 
     // CREATE TEXTURE
-    let textures = [create_texture(&gl), create_texture(&gl)];
+    let state = (*STATE).lock().unwrap();
+    let textures = [create_texture(&gl, &state), create_texture(&gl, &state)];
+    drop(state);
 
     // CREATE FRAMEBUFFER & ATTACH TEXTURE
     let framebuffer_objects = [
@@ -346,74 +400,86 @@ pub fn main() -> Result<(), JsValue> {
     let f = Rc::new(RefCell::new(None));
     let g = f.clone();
     *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
-        if unsafe { SHOULD_RENDER } {
+        // it's ok to borrow this as mutable for the entire block,
+        // since it is synchronous and no other function calls can
+        // try to lock the mutex while it is in use
+        let mut state = (*STATE).lock().unwrap();
+        if state.should_render {
             let now = window.performance().unwrap().now();
-            update_render_globals();
-            update_moving_fps_array(now);
+
+            update_render_globals(&mut state);
+            update_moving_fps_array(now, &mut state);
 
             // SET UNIFORMS
             gl.uniform1i(texture_u_location.as_ref(), 0);
-            gl.uniform1f(width_u_location.as_ref(), WIDTH as f32);
-            gl.uniform1f(height_u_location.as_ref(), HEIGHT as f32);
-            gl.uniform1i(max_depth_u_location.as_ref(), MAX_DEPTH as i32);
+            gl.uniform1f(width_u_location.as_ref(), state.width as f32);
+            gl.uniform1f(height_u_location.as_ref(), state.height as f32);
+            gl.uniform1i(max_depth_u_location.as_ref(), state.max_depth as i32);
             gl.uniform1f(time_u_location.as_ref(), now as f32);
             gl.uniform1i(
                 samples_per_pixel_u_location.as_ref(),
-                SAMPLES_PER_PIXEL as i32,
+                state.samples_per_pixel as i32,
             );
-            gl.uniform1f(aspect_ratio_u_location.as_ref(), ASPECT_RATIO as f32);
-            gl.uniform1f(viewport_height_u_location.as_ref(), *VIEWPORT_HEIGHT as f32);
-            gl.uniform1f(focal_length_u_location.as_ref(), FOCAL_LENGTH as f32);
+            gl.uniform1f(aspect_ratio_u_location.as_ref(), state.aspect_ratio as f32);
+            gl.uniform1f(
+                viewport_height_u_location.as_ref(),
+                state.viewport_height as f32,
+            );
+            gl.uniform1f(focal_length_u_location.as_ref(), state.focal_length as f32);
             gl.uniform3fv_with_f32_array(
                 camera_origin_u_location.as_ref(),
-                &CAMERA_ORIGIN.to_array(),
+                &state.camera_origin.to_array(),
             );
-            gl.uniform1f(viewport_width_u_location.as_ref(), *VIEWPORT_WIDTH as f32);
+            gl.uniform1f(
+                viewport_width_u_location.as_ref(),
+                state.viewport_width as f32,
+            );
             gl.uniform3fv_with_f32_array(
                 viewport_horizontal_vec_u_location.as_ref(),
-                &VIEWPORT_HORIZONTAL_VEC.to_array(),
+                &state.viewport_horizontal_vec.to_array(),
             );
             gl.uniform3fv_with_f32_array(
                 viewport_vertical_vec_u_location.as_ref(),
-                &VIEWPORT_VERTICAL_VEC.to_array(),
+                &state.viewport_vertical_vec.to_array(),
             );
             gl.uniform3fv_with_f32_array(
                 lower_left_corner_u_location.as_ref(),
-                &LOWER_LEFT_CORNER.to_array(),
+                &state.lower_left_corner.to_array(),
             );
-            gl.uniform1f(render_count_u_location.as_ref(), unsafe { RENDER_COUNT });
-            gl.uniform1i(should_average_u_location.as_ref(), SHOULD_AVERAGE as i32);
+            gl.uniform1f(render_count_u_location.as_ref(), state.render_count);
+            gl.uniform1i(
+                should_average_u_location.as_ref(),
+                state.should_average as i32,
+            );
             gl.uniform1f(
                 last_frame_weight_u_location.as_ref(),
-                LAST_FRAME_WEIGHT as f32,
+                state.last_frame_weight as f32,
             );
 
             // RENDER
             // use texture previously rendered to
             gl.bind_texture(
                 WebGl2RenderingContext::TEXTURE_2D,
-                Some(&textures[(unsafe { EVEN_ODD_COUNT + 1 } % 2) as usize]),
+                Some(&textures[((state.even_odd_count + 1) % 2) as usize]),
             );
 
             // draw to canvas
             gl.bind_framebuffer(WebGl2RenderingContext::FRAMEBUFFER, None);
-            draw(&gl);
+            draw(&gl, &state);
 
             // only need to draw to framebuffer when doing averages of previous frames
-            if SHOULD_AVERAGE {
+            if state.should_average {
                 // RENDER (TO FRAMEBUFFER)
                 gl.bind_framebuffer(
                     WebGl2RenderingContext::FRAMEBUFFER,
-                    Some(&framebuffer_objects[(unsafe { EVEN_ODD_COUNT } % 2) as usize]),
+                    Some(&framebuffer_objects[(state.even_odd_count % 2) as usize]),
                 );
-                draw(&gl);
+                draw(&gl, &state);
             }
 
             // if user has requested to save, save immediately after rendering
-            if unsafe { SHOULD_SAVE } {
-                unsafe {
-                    SHOULD_SAVE = false;
-                }
+            if state.should_save {
+                state.should_save = false;
                 let data_url = canvas
                     .to_data_url()
                     .unwrap()
@@ -429,12 +495,13 @@ pub fn main() -> Result<(), JsValue> {
                 a.click();
             }
 
-            update_fps_indicator(&fps_indicator, now);
+            update_fps_indicator(&fps_indicator, now, &mut state);
         }
-        request_animation_frame(f.borrow().as_ref().unwrap());
+
+        request_animation_frame((*f).borrow().as_ref().unwrap());
     }) as Box<dyn FnMut()>));
 
-    request_animation_frame(g.borrow().as_ref().unwrap());
+    request_animation_frame((*g).borrow().as_ref().unwrap());
 
     Ok(())
 }
